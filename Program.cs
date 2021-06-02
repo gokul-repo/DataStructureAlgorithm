477eed86fe14e14837b020f204ec7216a777710b
gokul-repo <gokul-repo@users.noreply.github.com>
2021-06-02 17:40:56 -0400

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,25 +8,25 @@ namespace DataStructureAlgorithm
     {
         static void Main(string[] args)
         {     
-             BinaryTrees myBT = new BinaryTrees();
+            BinaryTrees myBT = new BinaryTrees();
             myBT.Create();
             Console.WriteLine("--------IN ORDER----------");
             myBT.inOrder(myBT.RootNode);
-             Console.WriteLine("--------IN ORDER iterative----------");
-            myBT.inOrder(myBT.RootNode);
+            Console.WriteLine("--------IN ORDER iterative----------");
+            myBT.InOrderIterative(myBT.RootNode);
             Console.WriteLine("--------PRE ORDER----------");
             myBT.preOrder(myBT.RootNode);
-              Console.WriteLine("--------PRE ORDER Iterative----------");
-            myBT.preOrder(myBT.RootNode);
+            Console.WriteLine("--------PRE ORDER Iterative----------");
+            myBT.PreOrderIterative(myBT.RootNode);
             Console.WriteLine("--------POST ORDER----------");
             myBT.postOrder(myBT.RootNode);
             Console.WriteLine("--------Level ORDER----------");
             myBT.LevelOrder(myBT.RootNode);
 
-            Console.WriteLine("Total number of Node {0}",myBT.Count(myBT.RootNode));
-            Console.WriteLine("Total number of Leaf Node {0}",myBT.CountLeaf(myBT.RootNode));
-            Console.WriteLine("Total number of Non Leaf Node {0}",myBT.CountNonLeaf(myBT.RootNode));
-            Console.WriteLine("Height is {0}",myBT.Height(myBT.RootNode));
+            Console.WriteLine("Total number of Node {0}", myBT.Count(myBT.RootNode));
+            Console.WriteLine("Total number of Leaf Node {0}", myBT.CountLeaf(myBT.RootNode));
+            Console.WriteLine("Total number of Non Leaf Node {0}", myBT.CountNonLeaf(myBT.RootNode));
+            Console.WriteLine("Height is {0}", myBT.Height(myBT.RootNode));
 
         }
     }
