15342ee4b60838696f786f005ed9ed26b5212670
gokul-repo <gokul-repo@users.noreply.github.com>
2021-06-04 16:51:19 -0400

    BST - Delete

--- BinarySearchTree/BinarySearchTree.cs
+++ BinarySearchTree/BinarySearchTree.cs
@@ -6,91 +6,125 @@ namespace DataStructureAlgorithm.BinarySearchTree
     {
         public Node Root;
 
-        public BinarySearchTree(){
-            Root=null;
+        public BinarySearchTree()
+        {
+            Root = null;
         }
 
         //Search Recursive
-        public bool SearchRecursive (Node root, int target){
-            if (root == null){
+        public bool SearchRecursive(Node root, int target)
+        {
+            if (root == null)
+            {
                 return false;
-            }else if(target<root.Value){
-                return SearchRecursive(root.LeftChild,target);
             }
-            else if (target > root.Value){
-                return SearchRecursive(root.RightChild,target);
-            }else{
+            else if (target < root.Value)
+            {
+                return SearchRecursive(root.LeftChild, target);
+            }
+            else if (target > root.Value)
+            {
+                return SearchRecursive(root.RightChild, target);
+            }
+            else
+            {
                 return true;
             }
 
         }
 
-         //Search Iterative
-        public bool SearchIterative (Node root, int target){
-            if (root == null){
+        //Search Iterative
+        public bool SearchIterative(Node root, int target)
+        {
+            if (root == null)
+            {
                 return false;
             }
             Node curr = root;
 
-            while(curr!= null){
-                if(curr.Value==target){
+            while (curr != null)
+            {
+                if (curr.Value == target)
+                {
                     return true;
-                }else if ( target <curr.Value){
-                    curr=curr.LeftChild;
-                }else{
-                    curr=curr.RightChild;
+                }
+                else if (target < curr.Value)
+                {
+                    curr = curr.LeftChild;
+                }
+                else
+                {
+                    curr = curr.RightChild;
                 }
             }
             return false;
         }
 
-         //Insert Recursive
-        public Node InsertRecursive (Node root, int val){            
-            if (root == null){                
-                Node newNode = new Node(val);                
+        //Insert Recursive
+        public Node InsertRecursive(Node root, int val)
+        {
+            if (root == null)
+            {
+                Node newNode = new Node(val);
                 return newNode;
-            }else if(val<root.Value){
-                 root.LeftChild=InsertRecursive(root.LeftChild,val);
             }
-            else if (val > root.Value){
-                root.RightChild =InsertRecursive(root.RightChild,val);
+            else if (val < root.Value)
+            {
+                root.LeftChild = InsertRecursive(root.LeftChild, val);
+            }
+            else if (val > root.Value)
+            {
+                root.RightChild = InsertRecursive(root.RightChild, val);
             }
             return root;
 
         }
 
         //Insert Iterative
-        public void InsertIterative (Node root, int val){
+        public void InsertIterative(Node root, int val)
+        {
             Node newNode = new Node(val);
-            if (root == null){
-                Root=newNode;
+            if (root == null)
+            {
+                Root = newNode;
                 return;
             }
-            Node prev =null;
+            Node prev = null;
             Node curr = root;
 
-            while(curr!= null){
-                if(curr.Value==val){
+            while (curr != null)
+            {
+                if (curr.Value == val)
+                {
                     Console.WriteLine("Duplicate");
                     return;
-                }else if (val <curr.Value){
-                    prev=curr;
-                    curr=curr.LeftChild;
-                }else{
-                    prev=curr;
-                    curr=curr.RightChild;
+                }
+                else if (val < curr.Value)
+                {
+                    prev = curr;
+                    curr = curr.LeftChild;
+                }
+                else
+                {
+                    prev = curr;
+                    curr = curr.RightChild;
                 }
             }
-            if (val < prev.Value){
-                prev.LeftChild=newNode;
-            }else{
-                prev.RightChild=newNode;
-            }            
+            if (val < prev.Value)
+            {
+                prev.LeftChild = newNode;
+            }
+            else
+            {
+                prev.RightChild = newNode;
+            }
         }
 
         //InOrder Traversal
-         public void inOrder(Node root){
-            if(root==null){
+        public void inOrder(Node root)
+        {
+            if (root == null)
+            {
                 return;
             }
             inOrder(root.LeftChild);
@@ -98,35 +132,87 @@ namespace DataStructureAlgorithm.BinarySearchTree
             inOrder(root.RightChild);
         }
 
-       //InOrder Successor
-       public Node InOrderSuccessor(Node root){
-           if (root == null ){
-               return null;
-           }
-           Node prev=null;
-           Node curr=root.RightChild;
-           while (curr!=null)
-           {
-               prev=curr;
-               curr=curr.LeftChild;               
-           }
-           return prev;
-       }
+        //InOrder Successor
+        public Node InOrderSuccessor(Node root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            Node prev = null;
+            Node curr = root.RightChild;
+            while (curr != null)
+            {
+                prev = curr;
+                curr = curr.LeftChild;
+            }
+            return prev;
+        }
 
-       //InOrder Predeccesor
-       public Node InOrderPredecessor(Node root){
-           if (root == null ){
-               return null;
-           }
-           Node prev=null;
-           Node curr=root.LeftChild;
-           while (curr!=null)
-           {
-               prev=curr;
-               curr=curr.RightChild;               
-           }
-           return prev;
-       }
-                        
+        //InOrder Predeccesor
+        public Node InOrderPredecessor(Node root)
+        {
+            if (root == null)
+            {
+                return null;
+            }
+            Node prev = null;
+            Node curr = root.LeftChild;
+            while (curr != null)
+            {
+                prev = curr;
+                curr = curr.RightChild;
+            }
+            return prev;
+        }
+
+        //Height
+        public int Height(Node root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+            int x = Height(root.LeftChild);
+            int y = Height(root.RightChild);
+            if (x > y)
+            {
+                return x + 1;
+            }
+            else
+            {
+                return y + 1;
+            }
+        }
+
+        //Delete
+
+        public Node Delete(Node root, int val){
+            if (root == null){
+                return null;
+            }
+            if(root.LeftChild == null && root.RightChild ==null){
+                    root = null;
+                    return root;
+            }
+            
+            if(val<root.Value){
+                root.LeftChild=Delete(root.LeftChild,val);
+            }else if(val>root.Value){
+                root.RightChild=Delete(root.RightChild,val);
+            }else{
+                if (Height(root.LeftChild) > Height(root.RightChild)){
+                    Node pred = InOrderPredecessor(root);
+                    root.Value=pred.Value;
+                    root.LeftChild =Delete(root.LeftChild,pred.Value);
+                }else{
+                    Node Succ = InOrderSuccessor(root);
+                    root.Value=Succ.Value;
+                    root.RightChild=Delete(root.RightChild,Succ.Value);
+                }
+            }
+            return root;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,48 +13,77 @@ namespace DataStructureAlgorithm
         //5. Inorder Traversal
         //6. Inorder Predecessor
         //7. Inorder Successor
+        //8. Delete Recursive
+        //9. Height
+        //10. 
 
         //To DO :
-        //8. Delete Recursive
-        //9. Create from Post Order
+        //1. Delete Recursive (Redo)
+        //2. Create from Pre Order
         static void Main(string[] args)
-        {     
+        {
             BinarySearchTree.BinarySearchTree myBT = new BinarySearchTree.BinarySearchTree();
 
-            myBT.InsertIterative(myBT.Root,10);
-            myBT.InsertIterative(myBT.Root,5);
-            myBT.InsertIterative(myBT.Root,20);
-            myBT.InsertIterative(myBT.Root,8);
-            myBT.InsertIterative(myBT.Root,30);
+            myBT.InsertIterative(myBT.Root, 10);
+            myBT.InsertIterative(myBT.Root, 5);
+            myBT.InsertIterative(myBT.Root, 20);
+            myBT.InsertIterative(myBT.Root, 8);
+            myBT.InsertIterative(myBT.Root, 30);
             myBT.inOrder(myBT.Root);
-            Console.WriteLine("Search - {0}",myBT.SearchIterative(myBT.Root, 10));
-            Console.WriteLine("Search - {0}",myBT.SearchRecursive(myBT.Root, 5));
-            Console.WriteLine("Search - {0}",myBT.SearchIterative(myBT.Root, 40));
-            Console.WriteLine("Search - {0}",myBT.SearchRecursive(myBT.Root, 50));
+            Console.WriteLine("Search - {0}", myBT.SearchIterative(myBT.Root, 10));
+            Console.WriteLine("Search - {0}", myBT.SearchRecursive(myBT.Root, 5));
+            Console.WriteLine("Search - {0}", myBT.SearchIterative(myBT.Root, 40));
+            Console.WriteLine("Search - {0}", myBT.SearchRecursive(myBT.Root, 50));
 
-            myBT.InsertRecursive(myBT.Root,40);
-            myBT.InsertRecursive(myBT.Root,50);
-            myBT.InsertRecursive(myBT.Root,9);
-            myBT.InsertRecursive(myBT.Root,2);
-            myBT.InsertRecursive(myBT.Root,110);
-             myBT.inOrder(myBT.Root);
+            myBT.InsertRecursive(myBT.Root, 40);
+            myBT.InsertRecursive(myBT.Root, 50);
+            myBT.InsertRecursive(myBT.Root, 9);
+            myBT.InsertRecursive(myBT.Root, 2);
+            myBT.InsertRecursive(myBT.Root, 110);
+            myBT.inOrder(myBT.Root);
 
             Console.WriteLine("------------Successor/Predecessor----------");
-             BinarySearchTree.BinarySearchTree myBT2 = new BinarySearchTree.BinarySearchTree();
-             
-            myBT2.Root= myBT2.InsertRecursive(myBT2.Root,30);
-            myBT2.InsertRecursive(myBT2.Root,20);
-            myBT2.InsertRecursive(myBT2.Root,40);
-            myBT2.InsertRecursive(myBT2.Root,10);
-            myBT2.InsertRecursive(myBT2.Root,25);
-            myBT2.InsertRecursive(myBT2.Root,35);
-            myBT2.InsertRecursive(myBT2.Root,45);
-             myBT2.inOrder(myBT2.Root);
+            BinarySearchTree.BinarySearchTree myBT2 = new BinarySearchTree.BinarySearchTree();
 
-             var pre= myBT2.InOrderPredecessor(myBT2.Root);
-             var succ= myBT2.InOrderSuccessor(myBT2.Root);
-             Console.WriteLine("Predecessor - {0}", pre.Value);
-             Console.WriteLine("Successor - {0}",succ.Value);
+            myBT2.Root = myBT2.InsertRecursive(myBT2.Root, 30);
+            myBT2.InsertRecursive(myBT2.Root, 20);
+            myBT2.InsertRecursive(myBT2.Root, 40);
+            myBT2.InsertRecursive(myBT2.Root, 10);
+            myBT2.InsertRecursive(myBT2.Root, 25);
+            myBT2.InsertRecursive(myBT2.Root, 35);
+            myBT2.InsertRecursive(myBT2.Root, 45);
+            myBT2.inOrder(myBT2.Root);
+
+            var pre = myBT2.InOrderPredecessor(myBT2.Root);
+            var succ = myBT2.InOrderSuccessor(myBT2.Root);
+            Console.WriteLine("Predecessor - {0}", pre.Value);
+            Console.WriteLine("Successor - {0}", succ.Value);
+
+            Console.WriteLine("------------Delete----------");
+            BinarySearchTree.BinarySearchTree myBT3 = new BinarySearchTree.BinarySearchTree();
+            myBT3.Root = myBT3.InsertRecursive(myBT3.Root, 10);
+            myBT3.InsertRecursive(myBT3.Root, 5);
+            myBT3.InsertRecursive(myBT3.Root, 20);
+            myBT3.InsertRecursive(myBT3.Root, 8);
+            myBT3.InsertRecursive(myBT3.Root, 30);
+            Console.WriteLine("Before: ");
+            myBT3.inOrder(myBT3.Root);
+            Console.WriteLine("AFter: ");
+            myBT3.Delete(myBT3.Root, 10);
+            myBT3.inOrder(myBT3.Root);
+
+            Console.WriteLine("------------Delete2----------");
+            BinarySearchTree.BinarySearchTree myBT4 = new BinarySearchTree.BinarySearchTree();
+            myBT4.Root = myBT4.InsertRecursive(myBT4.Root, 50);
+            myBT4.InsertRecursive(myBT4.Root, 10);
+            myBT4.InsertRecursive(myBT4.Root, 40);
+            myBT4.InsertRecursive(myBT4.Root, 20);
+            myBT4.InsertRecursive(myBT4.Root, 30);
+            Console.WriteLine("Before: ");
+            myBT4.inOrder(myBT4.Root);
+            Console.WriteLine("AFter: ");
+            myBT4.Delete(myBT4.Root, 50);
+            myBT4.inOrder(myBT4.Root);
 
 
         }
