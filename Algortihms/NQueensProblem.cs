7813218d5708f1eb45974b0cb0c02c40bbaaab88
gokul-repo <gokul-repo@users.noreply.github.com>
2021-07-11 12:10:01 -0400

    N Queens with all possible solution

--- Algortihms/NQueensProblem.cs
+++ Algortihms/NQueensProblem.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace DataStructureAlgorithm.Algortihms
 {
@@ -17,9 +18,19 @@ namespace DataStructureAlgorithm.Algortihms
         public void NQueensProblem_main()
         {
             Position[] positions = SolveNQueen(4);
-            foreach(var pos in positions){
-                Console.WriteLine(pos.Row+","+pos.Column);
+            foreach (var pos in positions)
+            {
+                Console.WriteLine(pos.Row + "," + pos.Column);
             }
+            List<List<Position>> allPositions = SolveNQueenAllSolutions(4);
+            foreach (var item in allPositions)
+            {
+                foreach (var pos in item)
+                {
+                    Console.WriteLine(pos.Row + "," + pos.Column);
+                }
+            }
+
 
         }
 
@@ -47,7 +58,7 @@ namespace DataStructureAlgorithm.Algortihms
                 bool isSafe = true;
                 for (int queen = 0; queen < row; queen++)
                 {
-                    if (positions[queen].Row == row || positions[queen].Column ==col ||
+                    if (positions[queen].Row == row || positions[queen].Column == col ||
                         positions[queen].Row + positions[queen].Column == row + col ||
                         positions[queen].Row - positions[queen].Column == row - col)
                     {
@@ -56,14 +67,59 @@ namespace DataStructureAlgorithm.Algortihms
                     }
                 }
 
-                if(isSafe){
-                    positions[row]= new Position(row,col);
-                    if(SolveNQueenUtil(positions,n,row+1)){
+                if (isSafe)
+                {
+                    positions[row] = new Position(row, col);
+                    if (SolveNQueenUtil(positions, n, row + 1))
+                    {
                         return true;
                     }
                 }
             }
             return false;
+        }
+
+
+        public List<List<Position>> SolveNQueenAllSolutions(int n)
+        {
+            List<List<Position>> results = new List<List<Position>>();
+            Position[] positions = new Position[n];
+            SolveNQueenUtilAllSOlutions(results, positions, n, 0);
+            return results;
+        }
+
+        public void SolveNQueenUtilAllSOlutions(List<List<Position>> results, Position[] positions, int n, int row)
+        {
+            if (row == n)
+            {
+                List<Position> currentResult = new List<Position>();
+                foreach (var position in positions)
+                {
+                    currentResult.Add(position);
+                }
+                results.Add(currentResult);
+                return;
+            }
+            for (int col = 0; col < n; col++)
+            {
+                bool isSafe = true;
+                for (int queen = 0; queen < row; queen++)
+                {
+                    if (positions[queen].Row == row || positions[queen].Column == col ||
+                        positions[queen].Row + positions[queen].Column == row + col ||
+                        positions[queen].Row - positions[queen].Column == row - col)
+                    {
+                        isSafe = false;
+                        break;
+                    }
+                }
+
+                if (isSafe)
+                {
+                    positions[row] = new Position(row, col);
+                    SolveNQueenUtilAllSOlutions(results, positions, n, row + 1);
+                }
+            }
         }
     }
 }
